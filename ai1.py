94e8dce208548d7695fec782580bd524a72a6b88
giorgio-modeo <75391952+giorgio-modeo@users.noreply.github.com>
2022-11-21 17:52:15 +0100

    neural network & graphic
    insert a first neural network
    graphic for decrease error

--- ai1.py
+++ ai1.py
@@ -1,11 +1,8 @@
+#inteligenza artificiale
 import numpy as np
-
+import nn1
+import matplotlib.pyplot as plt
 input_vector = [1.72, 1.23]
-"""
-input_vector = []
-for i in range(1,3):
-    input_vector.append(float(input("inserisci i numeri:")))
-"""
 weights_1 = [1.26, 0]
 weights_2 = [2.17, 0.32]
 first_indexes_mult = input_vector[0] * weights_1[0]
@@ -38,3 +35,23 @@ weights_1 = weights_1 - derivative
 prediction = make_prediction(input_vector, weights_1, bias)
 error = (prediction - target) ** 2
 print(f"Prediction: {prediction}; Error: {error}")
+def sigmoid_deriv(x):
+     return sigmoid(x) * (1-sigmoid(x))
+derror_dprediction = 2 * (prediction - target)
+layer_1 = np.dot(input_vector, weights_1) + bias
+dprediction_dlayer1 = sigmoid_deriv(layer_1)
+dlayer1_dbias = 1
+derror_dbias = (
+     derror_dprediction * dprediction_dlayer1 * dlayer1_dbias
+ )
+learning_rate = 0.1
+neural_network = nn1.NeuralNetwork(learning_rate)
+neural_network.predict(input_vector)
+input_vectors = np.array([[3, 1.5],[2, 1],[4, 1.5],[3, 4],[3.5, 0.5],[2, 0.5],[5.5, 1],[1, 1],])
+targets = np.array([0, 1, 0, 1, 0, 1, 1, 0])
+neural_network = nn1.NeuralNetwork(learning_rate)
+training_error = neural_network.train(input_vectors, targets, 10000)
+plt.plot(training_error)
+plt.xlabel("Iterations")
+plt.ylabel("Error for all training instances")
+plt.savefig("cumulative_error.png")
