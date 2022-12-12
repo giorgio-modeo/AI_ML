1bd131aa0f902081f5031d64d67c4dfe292a346f
giorgio-modeo <75391952+giorgio-modeo@users.noreply.github.com>
2022-12-12 15:01:33 +0100

    inizzio

---
+++ AI2.py
@@ -0,0 +1,49 @@
+import numpy
+from tensorflow import keras
+from keras.constraints import maxnorm
+from keras.utils import np_utils
+from keras.datasets import cifar10
+
+seed = 21
+
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train = X_train / 255.0
+X_test = X_test / 255.0
+
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+class_num = y_test.shape[1]
+
+model = keras.Sequential([
+    keras.layers.layer1,
+    keras.layers.layer2,
+    keras.layers.layer3
+])
+
+model = keras.Sequential()
+model.add(keras.layers.Conv2D(32, (3, 3), input_shape=X_train.shape[1:], padding='same'))
+model.add(keras.layers.Activation('relu'))
+model.add(keras.layers.Conv2D(32, 3, input_shape=(32, 32, 3), activation='relu', padding='same'))
+
+# 0.2 = drops 20%
+model.add(keras.layers.Dropout(0.2))
+model.add(keras.layers.BatchNormalization())
+
+model.add(keras.layers.Conv2D(64, 3, activation='relu', padding='same'))
+model.add(keras.layers.MaxPooling2D(2))
+model.add(keras.layers.Dropout(0.2))
+model.add(keras.layers.BatchNormalization())
+    
+model.add(keras.layers.Conv2D(128, 3, activation='relu', padding='same'))
+model.add(keras.layers.Dropout(0.2))
+model.add(keras.layers.BatchNormalization())
+
+model.add(keras.layers.Conv2D(256, 3, activation='relu', padding='same'))
+model.add(keras.layers.Dropout(0.2))
+model.add(keras.layers.BatchNormalization())
+
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dropout(0.2))
