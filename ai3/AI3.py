6680f35eaebe5fdc3d253e2a824595617596b425
giorgio-modeo <75391952+giorgio-modeo@users.noreply.github.com>
2023-01-26 17:54:46 +0100

    adad

--- ai3/AI3.py
+++ ai3/AI3.py
@@ -8,8 +8,8 @@ neural_network = nn1.NeuralNetwork(learning_rate)
 neural_network.predict(input_vector)
 
 # gli imput devono essere proporzionali ai target
-input_vectors = np.array([[3, 1.5],[2, 1],[4, 1.5],[3, 4],[3.5, 0.5],[2, 0.5],[5.5, 1],[1, 1],])
-targets = np.array([0, 1, 0, 1, 0, 1, 1, 0, ])
+input_vectors = np.array([[3, 1.5],[2, 1],[4, 1.5],[3, 4],[3.5, 0.5],[2, 0.5],[5.5, 1],[1, 1],[26,1],])
+targets = np.array([0, 1, 0, 1, 0, 1, 1, 0, 1, ])
 
 neural_network = nn1.NeuralNetwork(learning_rate)
 training_error = neural_network.train(input_vectors, targets, 10000)
