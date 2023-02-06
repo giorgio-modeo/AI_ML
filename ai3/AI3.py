e11076afda2d46a9ef1018c0771129325780290e
Giorgio Bruno Modeo <75391952+giorgio-modeo@users.noreply.github.com>
2023-02-06 17:37:24 +0100

    ad

--- ai3/AI3.py
+++ ai3/AI3.py
@@ -1,17 +1,29 @@
 import sys
-sys.path.append("E:/py/python/nn1")
+sys.path.append("nn1")
 import nn1
 import numpy as np
 import matplotlib.pyplot as plt
 #     non so cosa succede qui
 input_vector = np.array([2, 1.5])
+print(input_vector)
 learning_rate = 0.01
 neural_network = nn1.NeuralNetwork(learning_rate)
 neural_network.predict(input_vector)
+# [2.  1.5]
 
 # gli imput devono essere proporzionali ai target
 input_vectors = np.array([[3, 1.5],[2, 1],[4, 1.5],[3, 4],[3.5, 0.5],[2, 0.5],[5.5, 1],[1, 1],[26,1],])
 targets = np.array([0, 1, 0, 1, 0, 1, 1, 0, 1, ])
+print(input_vectors)
+# [[ 3.   1.5]
+#  [ 2.   1. ]
+#  [ 4.   1.5]
+#  [ 3.   4. ]
+#  [ 3.5  0.5]
+#  [ 2.   0.5]
+#  [ 5.5  1. ]
+#  [ 1.   1. ]
+#  [26.   1. ]]
 
 neural_network = nn1.NeuralNetwork(learning_rate)
 training_error = neural_network.train(input_vectors, targets, 10000)
