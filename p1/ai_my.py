d0a71c0a5993c650b16ffd224d03183c6b1c0a8d
giorgio-modeo <75391952+giorgio-modeo@users.noreply.github.com>
2023-03-26 06:35:16 +0200

    restore

--- p1/ai_my.py
+++ p1/ai_my.py
@@ -1,3 +1,28 @@
+# import numpy as np
+# saluti = ['ciao','salve','buongiorno','buonasera']
+# nrandom = np.random.randint(len(saluti))
+
+
+# richiesta=input("richiesta: ")
+# richiestal=richiesta.split()
+# nstringa=int("C")
+# print(nstringa)
+# for i in saluti:
+#     for j in richiestal:
+#         if i == j:
+#             print(i)
+
+# text = input("enter a string to convert into ascii values:")
+# splt = text.split()
+# ascii_values = []
+# i =0
+
+# print()
+# for character in text:
+#     ascii_values.append(ord(splt[i]))
+#     i+=1
+# print(ascii_values)
+
 
 from PIL import Image
 import numpy as np
@@ -5,22 +30,22 @@ import nn2
 
 input_vector = np.array(Image.open('Z:/tutto/python/ai3/cumulative_error.png'))
 
+print(input_vector)
+
 #creo i target
 for x in range(len(input_vector)):
     x = x
-x+=1
+    pass
 targets = np.random.randint(2,size=x)
+
 learning_rate = 0.01
 neural_network = nn2.NeuralNetwork(learning_rate)
-a=neural_network.predict(input_vector)
-print(int(a[0][0]))
-
-image = Image.fromarray(a)
-image.save('prova_immagini/c_err_prova.png')
+neural_network.predict(input_vector)
+training_error = neural_network.train(input_vector, targets, 10000)
 
 # (480, 640, 4)
 
-# im_R = im.cop\y()
+# im_R = im.copy()
 # im_R[:, :, (1, 2)] = 0
 # im_G = im.copy()
 # im_G[:, :, (0, 2)] = 0
