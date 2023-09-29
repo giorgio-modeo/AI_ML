af9f40737aad4117dde332c897d7050d02c05a1f
giorgio-modeo <75391952+giorgio-modeo@users.noreply.github.com>
2023-09-29 07:14:58 +0200

    fine nn3 inizzio ai5

---
+++ nn1/ai5.py
@@ -0,0 +1,4 @@
+import nn3 
+
+
+neutal_network = nn3.NeuralNetwork(0.01)
--- nn1/nn3.py
+++ nn1/nn3.py
@@ -1,20 +1,13 @@
 import numpy as np
-from gensim.test.utils import common_texts
-from gensim.models import Word2Vec
-
-
-
-model = Word2Vec(sentences=common_texts, vector_size=100, window=5, min_count=1, workers=4)
-model.save("word2vec.model")
-
+from gensim.models.keyedvectors import KeyedVectors
 
 # Definizione della classe NeuralNetwork
 class NeuralNetwork:
     # Definizione del metodo init, che inizializza i pesi, il bias e il learning rate
     def __init__(self, learning_rate):
-        # Inizializzazione dei pesi con valori casuali
-        self.weights = Word2Vec.wv.vectors
-        self.weights_2 = Word2Vec.wv.vectors
+        # Inizializzazione dei pesi con i vettori di embedding del modello Word2Vec
+        self.weights = KeyedVectors.load_word2vec_format("nn1/word2vec.model")
+        self.weights_2 = KeyedVectors.load_word2vec_format("nn1/word2vec.model")
 
         self.bias = np.random.normal(loc=0.0,scale=0.01,size=(100,200))
         self.bias_2 = np.random.normal(loc=0.0,scale=0.01,size=(100,200))
@@ -60,25 +53,14 @@ class NeuralNetwork:
         prediction = layer_3
         return prediction
 
-
-    def forward(self,input_vector):
-        np.dot(input_vector, self.weights) + self.bias
-        output = np.tanh(output)
-        return output
-    
-    def loss_fun():
-        return
-
     # Definizione del metodo per il calcolo dei gradienti
     def _compute_gradients(self, input_vector, target):
-        # Calcolo dell'output del primo layer
-        layer_1 = forward(input_vector)
+    # Calcolo dell'output del primo layer
+        layer_1 = np.dot(input_vector, self.weights) + self.bias
         # Applicazione della funzione di attivazione sigmoid all'output del primo layer
-        layer_2 = forward(layer_1)
+        layer_2 = self._sigmoid(layer_1)
         # La previsione è l'output del secondo layer
         prediction = layer_2
-
-
         # Calcolo della derivata dell'errore rispetto alla previsione
         derror_dprediction = 2 * (prediction - target)
         # Calcolo della derivata dell'output del secondo layer rispetto all'output del primo layer
@@ -98,43 +80,45 @@ class NeuralNetwork:
         # Restituzione delle derivate
         return derror_dbias, derror_dweights
 
-    # Definizione del metodo per l'aggiornamento dei parametri
-    def _update_parameters(self, derror_dbias, derror_dweights):
-        # Aggiornamento del bias
-        self.bias = self.bias - (derror_dbias * self.learning_rate)
-        # Aggiornamento dei pesi
-        self.weights = self.weights - (
-            derror_dweights * self.learning_rate
+# Definizione del metodo per l'aggiornamento dei parametri
+def _update_parameters(self, derror_dbias, derror_dweights):
+    # Aggiornamento del bias
+    self.bias = self.bias - (derror_dbias * self.learning_rate)
+    # Aggiornamento dei pesi
+    self.weights = self.weights - (
+        derror_dweights * self.learning_rate
+    )
+
+def train(self, input_phrases, targets, iterations):
+    cumulative_errors = []
+    for current_iteration in range(iterations):
+        # Selezione casuale di un batch di dati di addestramento
+        batch_size = 32
+        random_indices = np.random.permutation(len(input_phrases))
+        random_indices = random_indices[:batch_size]
+        batch_input_phrases = [input_phrases[index] for index in random_indices]
+        batch_targets = [targets[index] for index in random_indices]
+
+        # Calcolo delle derivate parziali dell'errore rispetto ai parametri del modello
+        derror_dbias, derror_dweights = self._compute_gradients(
+            batch_input_phrases, batch_targets
         )
 
-    def train(self, input_vectors, targets, iterations):
-        cumulative_errors = []
-        for current_iteration in range(iterations):
-            # seleziona casualmente un dato di input e il suo obiettivo corrispondente
-            # La linea seguente estrae casualmente un indice di input_vectors e targets
-            # su cui effettuare la successiva iterazione del training
-            random_data_index = np.random.randint(len(input_vectors))
-            input_vector = input_vectors[random_data_index]
-            target = targets[random_data_index]
+        # Aggiornamento dei pesi e dei bias utilizzando le derivate calcolate
+        self._update_parameters(derror_dbias, derror_dweights)
 
-            # Calcola le derivate parziali dell'errore rispetto ai parametri del modello
-            derror_dbias, derror_dweights = self._compute_gradients(input_vector, target)
+        # Ogni 100 iterazioni, calcola l'errore cumulativo su tutto il dataset
+        if current_iteration % 100 == 0:
+            cumulative_error = 0
+            for data_instance_index in range(len(input_phrases)):
+                data_phrase = input_phrases[data_instance_index]
+                target = targets[data_instance_index]
+                prediction = self.predict(data_phrase)
+                error = np.square(prediction - target)
+                cumulative_error += error
+            cumulative_errors.append(cumulative_error)
 
-            # Aggiorna i pesi e il bias utilizzando le derivate calcolate in precedenza
-            self._update_parameters(derror_dbias, derror_dweights)
-
-            # Ogni 100 iterazioni, calcola l'errore cumulativo su tutto il dataset di input_vectors
-            if current_iteration % 100 == 0:
-                cumulative_error = 0
-                for data_instance_index in range(len(input_vectors)):
-                    data_point = input_vectors[data_instance_index]
-                    target = targets[data_instance_index]
-                    prediction = self.predict(data_point)
-                    error = np.square(prediction - target)
-                    cumulative_error += error
-
-                cumulative_errors.append(cumulative_error)
+    return cumulative_errors
 
 
-        return cumulative_errors
 
