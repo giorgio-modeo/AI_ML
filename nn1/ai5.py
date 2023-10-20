26862c19ce3dacf8b5f6031dc1add3ce831efc3c
giorgiomodeo-admin <giorgio.modeo@gmail.com>
2023-10-20 10:22:41 +0200

    commented creation of language model

--- nn1/ai5.py
+++ nn1/ai5.py
@@ -2,12 +2,13 @@ import nn3
 import gensim.models.word2vec as KeyedVectors2
 learning_rate = 0.1
 neural_network = nn3.NeuralNetwork(learning_rate)
-
-sentences = ["Ciao, come stai?", "Sto bene, grazie. E tu?", "Anch'io sto bene."]
-# Crea un nuovo modello Word2Vec
-model = KeyedVectors2.Word2Vec(sentences, vector_size=100, window=5, min_count=1)
-# Salva il modello
-model.save("word2vec.model")
+# ==================== create language model ====================
+# sentences = ["Ciao, come stai?", "Sto bene, grazie. E tu?", "Anch'io sto bene."]
+# # Crea un nuovo modello Word2Vec
+# model = KeyedVectors2.Word2Vec(sentences, vector_size=100, window=5, min_count=1)
+# # Salva il modello
+# model.save("word2vec.model")
+# ================================================================
 
 frase = 'ciao sono Giorgio e mi piacciono i treni'
 target= 'ciao Giorgio anche a me piacciono i treni'
